055f0535e2fb229b5789c063bad8e6be11cda905
Nguyễn Văn Hoạch <automails123@gmail.com>
2026-03-30 15:24:06 +0700

    update

---
+++ wp-content/themes/msb/theme/404.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package msb
+ */
+
+get_header();
+?>
+
+	<section id="primary">
+		<main id="main">
+
+			<div>
+				<header class="page-header">
+					<h1 class="page-title"><?php esc_html_e( 'Page Not Found', 'msb' ); ?></h1>
+				</header><!-- .page-header -->
+
+				<div <?php msb_content_class( 'page-content' ); ?>>
+					<p><?php esc_html_e( 'This page could not be found. It might have been removed or renamed, or it may never have existed.', 'msb' ); ?></p>
+					<?php get_search_form(); ?>
+				</div><!-- .page-content -->
+			</div>
+
+		</main><!-- #main -->
+	</section><!-- #primary -->
+
+<?php
+get_footer();
---
+++ wp-content/themes/msb/theme/template-parts/layout/footer-content.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Template part for displaying the footer content
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package msb
+ */
+
+?>
+
+<footer id="colophon">
+
+	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+		<aside role="complementary" aria-label="<?php esc_attr_e( 'Footer', 'msb' ); ?>">
+			<?php dynamic_sidebar( 'sidebar-1' ); ?>
+		</aside>
+	<?php endif; ?>
+
+	<?php if ( has_nav_menu( 'menu-2' ) ) : ?>
+		<nav aria-label="<?php esc_attr_e( 'Footer Menu', 'msb' ); ?>">
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'menu-2',
+					'menu_class'     => 'footer-menu',
+					'depth'          => 1,
+				)
+			);
+			?>
+		</nav>
+	<?php endif; ?>
+
+	<div>
+		<?php
+		$msb_blog_info = get_bloginfo( 'name' );
+		if ( ! empty( $msb_blog_info ) ) :
+			?>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>,
+			<?php
+		endif;
+
+		/* translators: 1: WordPress link, 2: WordPress. */
+		printf(
+			'<a href="%1$s">proudly powered by %2$s</a>.',
+			esc_url( __( 'https://wordpress.org/', 'msb' ) ),
+			'WordPress'
+		);
+		?>
+	</div>
+
+</footer><!-- #colophon -->
